9472d94c0dedfda6e27640ec7b6cc6a0c847a305
Tyler McKell <tmckell@devpipeline.com>
2024-01-19 08:52:45 -0700

    fixed status codes

--- basic flask app/controllers/products_controller.py
+++ basic flask app/controllers/products_controller.py
@@ -26,14 +26,14 @@ def read_all_products():
 def read_products_active():
     for product in product_records:
         if product['active'] == True:
-            return jsonify({'message': 'product is active', 'results': product}), 201
+            return jsonify({'message': 'product is active', 'results': product}), 200
     return jsonify({'message': f'{product} is not active'}), 500
 
 
 def read_products_by_id(id):
     for product in product_records:
         if product['product_id'] == int(id):
-            return jsonify({'message': 'product found', 'results': product}), 201
+            return jsonify({'message': 'product found', 'results': product}), 200
     return jsonify({'message': f'Product with id {id} not found'}), 404
 
 
@@ -50,7 +50,7 @@ def update_product(id):
     product['description'] = data.get('description', product['description'])
     product['price'] = data.get('price', product['price'])
 
-    return jsonify({'message': 'product updated', 'results': product}), 201
+    return jsonify({'message': 'product updated', 'results': product}), 200
 
 
 # delete function
@@ -59,4 +59,4 @@ def delete_product(id):
     for record in product_records:
         if record['product_id'] == int(id):
             product_records.remove(record)
-    return jsonify({'message': f'product {id} has been removed successfully'}), 201
+    return jsonify({'message': f'product {id} has been removed successfully'}), 200
